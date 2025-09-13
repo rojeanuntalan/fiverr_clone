806876cb04abdfbb4d57948332f845691a0232be
rojeanuntalan <rojean.untalan.cvt@eac.edu.ph>
2025-09-13 10:11:50 +0800

    Add files via upload

---
+++ client/project_offers_submitted.php
@@ -0,0 +1,37 @@
+<?php require_once 'classloader.php'; ?>
+<?php 
+if (!$userObj->isLoggedIn()) {
+  header("Location: login.php");
+}
+
+if (!$userObj->isAdmin()) {
+  header("Location: ../freelancer/index.php");
+} 
+?>
+<!doctype html>
+  <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <style>
+      body {
+        font-family: "Arial";
+      }
+    </style>
+  </head>
+  <body>
+    <?php include 'includes/navbar.php'; ?>
+    <div class="container-fluid">
+      <div class="display-4 text-center">Hello there and welcome! Here are all the submitted project offers!</div>
+      <div class="row justify-content-center">
+        <div class="col-md-12">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
